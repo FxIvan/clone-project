08368341ddacd4dac4412f6c63d5c597d1d8f290
FxIvan <almendraivan210814@gmail.com>
2023-05-19 14:56:58 -0300

    agregando imagen a las card

--- component/app/card/index.js
+++ component/app/card/index.js
@@ -10,6 +10,10 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
+import taunt from "../../../public/image/landing/TAUNT.jpg";
+import icon2 from "../../../public/image/landing/iconbox2.jpg";
+import Image from "next/image";
+
 export default function Card({
   projectStatus,
   swapRate,
@@ -20,12 +24,27 @@ export default function Card({
 }) {
   return (
     <Col sm={5} className={styles.containerCard + " p-4 mt-4"}>
-      <Link href="#">
-        <Row>
-          <Col sm={12}>
-            <Row>
-              <Col sm={4}>IMAGE</Col>
-              <Col sm={8}>
+      <Row>
+        <Col sm={12}>
+          <Row>
+            <Col sm={4} className="d-flex">
+              <div className={styles.iconBox}>
+                {/*
+                <div className={styles.spinnerContainer}>
+                  <div className={styles.spinnerBlock}>
+                    <div
+                      className={`${styles.spinner}  ${styles.spinnerOne}`}
+                    ></div>
+                  </div>
+                </div>
+                */}
+                <span>
+                  <Image src={icon2} />
+                </span>
+              </div>
+            </Col>
+            <Col sm={8}>
+              <Link href="#">
                 <div
                   className={
                     styles.containerHeaderCards + " d-flex flex-column"
@@ -90,59 +109,59 @@ export default function Card({
                     <Button className="rounded-pill">BUSD</Button>
                   </div>
                 </div>
-              </Col>
-            </Row>
-          </Col>
-          <Col sm={12} className="py-3">
-            <div className={styles.descriptionCard}>
-              <p>{description}</p>
-            </div>
-          </Col>
-          <Col sm={12}>
-            <div className={styles.swapCapAccess}>
-              <ol className="d-flex justify-content-between">
-                <div className="text-left">
-                  <li>Swap rate</li>
-                  <li>
-                    <span>{swapRate}</span>
-                  </li>
-                </div>
-                <div className="text-center">
-                  <li>Cap</li>
-                  <li>
-                    <span>{cap}</span>
-                  </li>
-                </div>
-                <div>
-                  <li className="text-end">Access</li>
-                  <li>
-                    <span>Private</span>
-                  </li>
-                </div>
-              </ol>
-            </div>
-          </Col>
-          <Col sm={12} className={styles.containerProgressBar}>
-            <div className="d-flex justify-content-between">
-              <p className="mb-1">Progress</p>
-              <p className="mb-1">
-                Participants: <span>{participant}</span>
-              </p>
-            </div>
-            <div>
-              <ProgressBar variant="warning" now={60} />
-            </div>
-            <div className="d-flex justify-content-between">
-              <p>
-                <span>0%</span>
-              </p>
-              <p>
-                <span>0/1000</span>
-              </p>
-            </div>
-          </Col>
-        </Row>
-      </Link>
+              </Link>
+            </Col>
+          </Row>
+        </Col>
+        <Col sm={12} className="py-3">
+          <div className={styles.descriptionCard}>
+            <p>{description}</p>
+          </div>
+        </Col>
+        <Col sm={12}>
+          <div className={styles.swapCapAccess}>
+            <ol className="d-flex justify-content-between">
+              <div className="text-left">
+                <li>Swap rate</li>
+                <li>
+                  <span>{swapRate}</span>
+                </li>
+              </div>
+              <div className="text-center">
+                <li>Cap</li>
+                <li>
+                  <span>{cap}</span>
+                </li>
+              </div>
+              <div>
+                <li className="text-end">Access</li>
+                <li>
+                  <span>Private</span>
+                </li>
+              </div>
+            </ol>
+          </div>
+        </Col>
+        <Col sm={12} className={styles.containerProgressBar}>
+          <div className="d-flex justify-content-between">
+            <p className="mb-1">Progress</p>
+            <p className="mb-1">
+              Participants: <span>{participant}</span>
+            </p>
+          </div>
+          <div>
+            <ProgressBar variant="warning" now={60} />
+          </div>
+          <div className="d-flex justify-content-between">
+            <p>
+              <span>0%</span>
+            </p>
+            <p>
+              <span>0/1000</span>
+            </p>
+          </div>
+        </Col>
+      </Row>
     </Col>
   );
 }
